6f8e2513fedb6b2564e7971c38f25f4571308d68
Fedor Garin <fedor@pymetrics.com>
2017-04-20 23:14:51 -0400

    finishes sample app with redux functionality for github

--- app/frontend/react/containers/FrontendAppContainer.jsx
+++ app/frontend/react/containers/FrontendAppContainer.jsx
@@ -1,32 +1,73 @@
 import React from "react"
+import Radium from "radium"
 
 import { connect } from "react-redux"
 
 import * as counterActions from "../actions/counterActions"
+import * as githubActions from "../actions/githubActions"
 import Headline from "../components/Headline"
+import GithubRepos from "../components/GithubRepos"
+
+const styles = {
+  button: {
+    cursor: "pointer",
+  },
+  counter: {
+    color: "blue",
+    fontSize: "20px",
+  }
+}
 
 @connect(state => ({
   counters: state.counters,
+  github: state.github,
 }))
-export default class SampleAppContainer extends React.Component {
+
+@Radium
+export default class FrontendAppContainer extends React.Component {
   handleClick() {
     let {dispatch} = this.props;
     dispatch(counterActions.increaseCounter())
   }
 
   render() {
-    let {counters} = this.props
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-12">
-            <Headline>Sample App!</Headline>
-            <div onClick={() => this.handleClick()}>INCREASE</div>
-            <p>{counters.clicks}</p>
-            <p>{process.env.BASE_API_URL}</p>
+      let {counters, github} = this.props
+      if (github.isLoadingRepos || github.repos === undefined) {
+        return this.renderLoading()
+      }
+      return (
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <Headline>Sample App!</Headline>
+              <div style={[styles.button]} onClick={() => this.handleClick()}>INCREASE</div>
+              <p style={[styles.counter]}>{counters.clicks}</p>
+              <p>{process.env.BASE_API_URL}</p>
+              {github.repos !== undefined &&
+                <GithubRepos repos={github.repos} />
+              }
+            </div>
           </div>
         </div>
-      </div>
-    )
-  }
+      )
+    }
+  
+  componentDidMount() {
+      let {dispatch, github} = this.props
+      if (!github.isLoadingRepos && github.repos === undefined) {
+        dispatch(githubActions.fetchRepos())
+      }
+    }
+    
+    renderLoading() {
+      return (
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              Loading...
+            </div>
+          </div>
+        </div>
+      )
+    }
 }
